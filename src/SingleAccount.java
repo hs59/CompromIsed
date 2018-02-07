04b21634f9036dd015c41fec6d6b2d79934c8c52
Hunter Scheven <hunterscheven@gmail.com>
2018-02-07 14:30:13 -0800

    Added a new class for handling email accounts instead of just domains. Updated the setRequestProperty value in SingleAccount.java too

--- src/SingleAccount.java
+++ src/SingleAccount.java
@@ -1,38 +1,23 @@
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
-import com.google.gson.*;
-import org.json.simple.JSONObject;
 
-/**     Getting data from a single breached site.
- *
- *      Format: GET https://haveibeenpwned.com/api/v2/breach/{name}
+/**
+ *  Similar to SingleSite, but for an email address instead of a website
  */
-
-public class SingleSite {
-    // JSON properties to be converted to Java
-    private String Title;
-    private String Name;
-    private String Domain;
-    private String BreachDate;
-    private String AddedDate;
-    private String PwnCount;
-    private String Description;
-    private boolean IsVerified;
-    private boolean IsFabricated;
-    private boolean IsSensitive;
-    private boolean IsActive;
-    private boolean IsRetired;
-    private boolean IsSpamList;
-    private JsonObject Object;
-
+public class SingleAccount {
     public static void Connection(String name) {
         try {
             // New URL object with the URL set - next few lines are all using native java lib
-            URL url = new URL("https://haveibeenpwned.com/api/v2/breach/" + name);
+            URL url = new URL("https://haveibeenpwned.com/api/v2/breachedaccount/" + name);
             // New HttpURLConnection object, opening the url
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             // Setting the User Agent - otherwise, Error 403 (See docs)
@@ -46,7 +31,7 @@ public class SingleSite {
             // Test reponseCode - continue if good
             if(responseCode != 200) {
                 if(responseCode == 404) {
-                    System.out.println("Not found - the domain could not be found and has " +
+                    System.out.println("Not found - the account could not be found and has " +
                             "therefore not been pwned.");
                     System.exit(1);
                 }
@@ -61,17 +46,13 @@ public class SingleSite {
             JsonElement root = jp.parse(new InputStreamReader((InputStream) conn.getContent()));
             JsonObject rootobj = root.getAsJsonObject();
 
-
+            SingleSite hhh = new SingleSite();
 
             String title = rootobj.get("Title").getAsString();
             String domain = rootobj.get("Domain").getAsString();
             String breachDate = rootobj.get("BreachDate").getAsString();
             String pwnCount = rootobj.get("PwnCount").getAsString();
             Boolean isVerified = rootobj.get("IsVerified").getAsBoolean();
-
-            Messages msg = new Messages();
-            msg.singleSite1();
-            formatStrings(title, domain, breachDate, pwnCount, isVerified);
 
             // Disconect from connection
             conn.disconnect();
@@ -83,22 +64,5 @@ public class SingleSite {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public static void formatStrings(String title, String domain, String breachDate,
-                                        String pwnCount, Boolean isVerified) {
-        String answer = "";
-        if(isVerified == true) {
-            answer = "Yes, it is verified.";
-        }
-        else {
-            answer = "No, it is not verified.";
-        }
-
-        System.out.println("- Title: " + title);
-        System.out.println("- Domain: " + domain);
-        System.out.println("- When was it breached?: " + breachDate);
-        System.out.println("- Pwn Count: " + pwnCount);
-        System.out.println("- Is it verified?: " + answer);
     }
 }
